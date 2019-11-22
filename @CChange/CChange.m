81fb7a75d8305718b6047a6e5bbdef276a979ea4
0xFFeng-jpg <58059879+0xFFeng-jpg@users.noreply.github.com>
2019-11-22 11:59:27 +0800

    Add files via upload

---
+++ @CChange/CChange.m
@@ -0,0 +1,19 @@
+classdef CChange
+    properties
+      data
+    end
+    
+    properties
+       para
+    end
+    
+    
+    methods
+        function obj = CChange(para,data)
+            obj.para = para;
+            obj.data = data;
+        end
+    end
+    
+    
+    
